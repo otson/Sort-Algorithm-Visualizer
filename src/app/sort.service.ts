3c845d9edeb257d3da1f3fddfd8e1c5cb3653a62
otson <59139705+otson@users.noreply.github.com>
2021-10-09 20:24:29 +0300

    add bubblesort algorithm, add reset and sort buttons

--- src/app/sort.service.ts
+++ src/app/sort.service.ts
@@ -10,8 +10,36 @@ export class SortService {
   public array: number[] = [];
 
   constructor() {
+    this.reset();
+  }
+  public bubbleSort(){
+    let sorted = false;
+    let round = 0;
+    console.log(this.array);
+    while(!sorted){
+      sorted = true;
+      for(let i = 0; i < this.array.length - 1 - round; i++){
+        if(this.array[i] > this.array[i+1]){
+          let temp = this.array[i];
+          this.array[i] = this.array[i+1];
+          this.array[i+1] = temp;
+          sorted = false;
+        }
+      }
+
+      round++;
+    }
+    console.log(this.array);
+  }
+
+  public reset() {
+    this.array = [];
     for(let i = 0; i < this.columns; i++){
       this.array.push(Math.ceil(Math.random()*this.maxHeight));
     }
+  }
+
+  public sort(){
+    this.bubbleSort();
   }
 }
